31a054d8d9ccf2e9fd2eb2caef65ed1c9f7f88f3
Russell Snyder <russellsnyder@Russells-MacBook-Pro-2.fritz.box>
2020-11-08 17:07:39 +0100

    fetch initial data from dropbox

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,24 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
+import dropboxV2Api from 'dropbox-v2-api';
 
-export default function Home() {
+function Home({ seo, main }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Ragin' Diarrhea | Homepage</title>
+        <title>{seo.title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div id="fb-root"/>
-      <script async defer crossOrigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v8.0&appId=658342791545843" nonce="HqqiIP7n"></script>
+      <div id="fb-root" />
+      <script async defer samesite="true" crossOrigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v8.0&appId=658342791545843" nonce="HqqiIP7n"></script>
 
       <main className={`${styles.main} container-fluid`}>
-        <h1 className={styles.title}>
-          Ragin Diarrhea 
-        </h1>
+        <h1 className={styles.title}>{main.band}</h1>
 
 
         <div className="row mb-5">
           <div className="col-sm-6">
-            <h2 className={styles.tagline}>
-              Circus Ska aus Herzogenaurach
-            </h2>
+            <h2 className={styles.tagline}>{main.tagline}</h2>
           </div>
           <div className="col-sm-6">
             <div className="fb-page" data-href="https://www.facebook.com/theragind" data-tabs="timeline" data-width="" data-height="300px" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/theragind" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/theragind">Ragin&#039; Diarrhea Facebook Page</a></blockquote></div>
@@ -48,3 +45,37 @@ export default function Home() {
     </div>
   )
 }
+
+export async function getStaticProps() {
+  const dropbox = dropboxV2Api.authenticate({
+    token: process.env.DROPBOX_TOKEN
+  })
+
+
+  const response = await dropbox({
+    resource: 'files/download',
+    parameters: {
+      path: '/pages/home/EN.json'
+    }
+  })
+
+  // const reader = new FileReader();
+  // console.log(response)
+  const data = await new Promise(function(resolve,reject) {
+    response.on('readable', () => {
+      response.setEncoding('utf8');
+      const json = response.read()
+      console.log(json);
+      resolve(JSON.parse(json))
+    })
+  })
+  
+  
+  return {
+    props: data
+  }
+
+}
+
+
+export default Home
