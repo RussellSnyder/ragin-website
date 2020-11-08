e00bd996d458cf13cd118bb99f7426316392afee
Russell Snyder <russellsnyder@Russells-MacBook-Pro-2.local>
2020-11-08 20:07:20 +0100

    ready for launch

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,67 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 import dropboxV2Api from 'dropbox-v2-api';
+import { FacebookProvider, Page } from 'react-facebook';
+import BandcampPlayer from 'react-bandcamp'
+import SpotifyPlayer from 'react-spotify-player';
 
-function Home({ seo, main }) {
+
+function Home({ seo, main, sections }) {
+  const { music, about, contact } = sections
+
   return (
     <div className={styles.container}>
       <Head>
         <title>{seo.title}</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta description={seo.description} />
+        <meta name="robots" content="index, follow"/>
       </Head>
-      <div id="fb-root" />
-      <script async defer samesite="true" crossOrigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v8.0&appId=658342791545843" nonce="HqqiIP7n"></script>
+      {/* <div id="fb-root" ></div>
+      <script async defer samesite="true" crossOrigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v8.0&appId=658342791545843" nonce="HqqiIP7n"></script> */}
 
       <main className={`${styles.main} container-fluid`}>
         <h1 className={styles.title}>{main.band}</h1>
-
-
-        <div className="row mb-5">
-          <div className="col-sm-6">
-            <h2 className={styles.tagline}>{main.tagline}</h2>
-          </div>
-          <div className="col-sm-6">
-            <div className="fb-page" data-href="https://www.facebook.com/theragind" data-tabs="timeline" data-width="" data-height="300px" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/theragind" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/theragind">Ragin&#039; Diarrhea Facebook Page</a></blockquote></div>
+        <h2 className={styles.tagline}>{main.tagline}</h2>
+        <div className="row mb-5 mt-5">
+          <div className="col-12 d-flex justify-content-center">
+            <FacebookProvider appId="658342791545843">
+              <Page href="https://www.facebook.com/theragind" tabs="timeline" width="700" />
+            </FacebookProvider>
           </div>
         </div>
-
         <div className="row mb-5">
-          <h2>Music</h2>
+          <div className="col-12 text-center text-md-left">
+            <h2>{music.title}</h2>
+          </div>
+          <div className="col-md-6">
+            <SpotifyPlayer
+              uri="spotify:album:0bt5XtAaiL8qIhCqAC3gXJ"
+              size={{
+                width: '100%',
+                height: '600px'
+              }}
+              view='coverart'
+              theme='black'
+            />
+          </div>
+          <div className="col-md-6">
+            <BandcampPlayer height="600px" width="500px" album={music.bandcamp.album} />
+          </div>
         </div>
 
-        <div className="row mb-5">
-          <h2>Peeps</h2>
+        <div className="row text-center text-md-left">
+          <div className="col-12">
+            <h2>{contact.title}</h2>
+          </div>
+          <h3 className="col-md-6">
+            {contact.message}
+          </h3>
+          <h3 className="col-md-6">
+            <a className="btn-block btn btn-primary-outline btn-primary btn-lg" href={`mailto:${contact.email}?subject=${contact.subject}`}>{contact.cta}</a>
+          </h3>
         </div>
-
-        <div className="row mb-5">
-          <h2>Contact</h2>
-        </div>
-
       </main>
 
       <footer className={styles.footer}>
@@ -61,7 +86,7 @@ export async function getStaticProps() {
 
   // const reader = new FileReader();
   // console.log(response)
-  const data = await new Promise(function(resolve,reject) {
+  const data = await new Promise(function (resolve, reject) {
     response.on('readable', () => {
       response.setEncoding('utf8');
       const json = response.read()
@@ -69,8 +94,8 @@ export async function getStaticProps() {
       resolve(JSON.parse(json))
     })
   })
-  
-  
+
+
   return {
     props: data
   }
